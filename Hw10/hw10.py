a36ee817e1cd264f4e79218fb6de30023ee0a4f6
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-05-09 21:23:29 +0300

    Update hw10.py

--- Hw10/hw10.py
+++ Hw10/hw10.py
@@ -1,4 +1,27 @@
 
 
-def find_bl_(str1: str)->bool:
-    
+def check_str_brackets(str1: str) -> bool:
+    """
+    Перевіряє що у рядку дужки парні.
+    Args:
+        str1 (str): рядок для перевірки 
+
+    Returns:
+        bool
+    """
+    list1 = []
+    brackets_dict = {'[': ']', '{': '}', '(': ')'}
+
+    for el in str1:
+        if el in brackets_dict.keys():
+            list1.append(el)
+        elif el in brackets_dict.values():
+            if not list1 or brackets_dict[list1.pop()] != el:
+                return False
+    return not list1
+
+
+# print(check_str_brackets("{}{}{}()()()[({})]")) - True
+# print(check_str_brackets(" {[}{}{}()(])()[(}}")) - False
+# print(check_str_brackets("фівфів0213"))
+
