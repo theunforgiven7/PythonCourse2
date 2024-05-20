c145346844bf87e1c4e21c5acc3c5b33dcf87c9d
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-05-20 17:07:39 +0300

    Update hw13.py

---
+++ Hw13/hw13.py
@@ -0,0 +1,6 @@
+class MobPhone:
+    def __init__(self, brand, size_h, size_w, price):
+        self.brand = brand
+        self.setSizeH(size_h)
+        self.setSizeW(size_w)
+        self.setPrice(price)
