574174b07e844f0b62666c53d23845af3c249deb
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-05-27 20:11:21 +0300

    Update hw14.py

--- Hw14/hw14.py
+++ Hw14/hw14.py
@@ -1,6 +1,14 @@
+from datetime import datetime
+
+def sort_func_train():
+    
+
+
 
 class Train:
-    def __init__(self, item, num_train, date_t):
-    self.item = item
-    self.num_train = num_train
-    self.date_t = date_t
+    def _init_(self, item, num_train, date_t):
+        self.item = item
+        self.num_train = num_train
+        self.date_t = date_t
+        if date_t < datetime.now():
+            return ('date_t не може бути меньшим за поточний час')
