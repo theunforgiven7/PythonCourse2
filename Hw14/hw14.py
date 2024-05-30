28c40c05927188a749995883b6de12b7e7b43f44
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-05-30 18:47:34 +0300

    Update hw14.py

--- Hw14/hw14.py
+++ Hw14/hw14.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class Train:
-    def _init_(self, item: int, num_train: int, date_t: int):
+    def _init_(self, item: int, num_train: int, date_t: datetime):
         self.item = item
         self.num_train = num_train
         self.date_t = date_t
@@ -13,4 +13,7 @@ class Train:
             return ('date_t не може бути меньшим за поточний час')
 
     def sort_func_train(num_train):
-        return sorted(num_train, key=lambda train: (train.destination, train.time))
+        return sorted(
+            num_train, key=lambda num_train:
+                (num_train.destination, num_train.departure_time)
+        )
