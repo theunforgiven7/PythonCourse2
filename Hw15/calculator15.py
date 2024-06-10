9c6b7f7c130b1b532eac184fca8c672460d56f51
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-06-10 12:18:55 +0300

    Update calculator15.py

--- Hw15/calculator15.py
+++ Hw15/calculator15.py
@@ -1,12 +1,30 @@
+class ValueZeroError(Exception):
+    """
+    Клас виключення класа Calculator
+    Виключення "Ділення на нуль"
+    """
+    def __init__(self):
+        """
+        Метод створення об'єкта класу виключення
+        "Ділення на нуль"
+        Args:
+            num2(int): 0
+        """
+        self.message = (f'помилка ділення на "0"')
+
+    def __str__(self) -> str:
+        """
+        Рядкове представлдення об'єкта класу виключення
+        return: опис виключення
+        """
+        return self.message
 
 
 class Calculator:
-    def __init__(self, num: int, num2: int) -> None:
-        self.num = num
-        self.num2 = num2
+    def __init__(self) -> None:
         self.results_dict: dict = {}
 
-    def __add__(self, num: int, num2: int):
+    def __add__(self, num: int, num2: int) -> None:
         result = num + num2
         self.results_dict[f"{num} + {num2}"] = result
         # print(self.results_dict)
@@ -15,48 +33,47 @@ class Calculator:
     def subtraction(self, num: int, num2: int):
         result = num - num2
         self.results_dict[f"{num} - {num2}"] = result
-        print(self.results_dict)
+        # print(self.results_dict)
 
-    def division(self, num: int, num2: int):
+    def division(self, num: int, num2: int) -> None:
         if num2 == 0:
-            raise ValueError("Division by zero is not allowed")
-        result = num / num2
-        self.results_dict[f"{num} / {num2}"] = result
-        print(self.results_dict)
+            raise ValueZeroError("You can't divide by zero")
+        else:
+            result = num / num2
+            self.results_dict[f"{num} / {num2}"] = result
+            # print(self.results_dict)
 
-    def multiplication(self, num: int, num2: int):
+    def multiplication(self, num: int, num2: int) -> None:
         result = num * num2
         self.results_dict[f"{num} * {num2}"] = result
+
+    def integer_division(self, num: int, num2: int) -> None:
+        if num2 == 0:
+            raise ValueZeroError("You can't divide by zero")
+        else:
+            result = num // num2
+            self.results_dict[f"{num} // {num2}"] = result
+
+    def remainder_from_division(self, num: int, num2: int) -> None:
+        if num2 == 0:
+            raise ValueZeroError("You can't divide by zero")
+        else:
+            result = num % num2
+            self.results_dict[f"{num} % {num2}"] = result
         # print(self.results_dict)
-    def get(self):
+
+    def exponentiation(self, num: int, num2: int) -> None:
+        result = pow(num, num2)
+        self.results_dict[f"{num} ** {num2}"] = result
+        # print(self.results_dict)
+
+    def get(self) -> dict:
         return self.results_dict
 
-a = Calculator(12, 0)
-a.division()
-# a.__add__(12,30)
 
-# a.get()
+a = Calculator()
+a.division(12, 9)
+a.division(12, 9)
+a.__add__(12,30)
 
-
-class ValueError(Exception):
-    """
-    Клас виключення класа Calculator
-    Виключення "Ділення на нуль"
-    """
-    def __init__(self, num2: int):
-        """
-        Метод створення об'єкта класу виключення
-        "Ділення на нуль"
-        Args:
-            num2(int): 0
-        """
-        self.message = (
-            f'помилка ділення на - {num2}'
-            )
-
-    def __str__(self) -> str:
-        """
-        Рядкове представлдення об'єкта класу виключення
-        return: опис виключення
-        """
-        return self.message
+print(a.get())
