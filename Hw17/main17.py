6bd18cd09bf7b995fe8b26c41cc5231ad64ff012
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-06-10 22:29:48 +0300

    Add files via upload

---
+++ Hw17/main17.py
@@ -0,0 +1,41 @@
+import sqlite3
+from sqlite3 import connect
+
+
+def create_database(database_name):
+    conn = sqlite3.connect(database_name + '.sqlite3')
+    conn.close()
+
+
+database_name = input("Надайте назву для нової бази даних: ")
+create_database(database_name)
+
+with connect('C:/Users/mariy/Downloads/tes.db') as con:
+    cur = con.cursor()
+
+    cur.execute('''CREATE TABLE "Table_1" (
+        "id"	INTEGER,
+        "name"	TEXT,
+        "num"	INTEGER,
+        "date"	INTEGER,
+        "big_num"	INTEGER,
+        PRIMARY KEY("id" AUTOINCREMENT)
+)''')
+
+    cur.execute('''CREATE TABLE "Table_2" (
+        "id"	INTEGER,
+        "name"	TEXT UNIQUE,
+        "num"	INTEGER,
+        "date"	INTEGER,
+        "big_num"	INTEGER,
+        PRIMARY KEY("id" AUTOINCREMENT)
+)''')
+
+    cur.execute('''CREATE TABLE "Table_3" (
+        "id"	INTEGER,
+        "name"	TEXT UNIQUE,
+        "num"	INTEGER NOT NULL,
+        "date"	INTEGER,
+        "big_num"	INTEGER,
+        PRIMARY KEY("id" AUTOINCREMENT)
+)''')
