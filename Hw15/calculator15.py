1517f7ece1d70536b48dc938c4de148119f72cd4
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-06-10 15:50:01 +0300

    Update calculator15.py

--- Hw15/calculator15.py
+++ Hw15/calculator15.py
@@ -6,11 +6,9 @@ class ValueZeroError(Exception):
     def __init__(self):
         """
         Метод створення об'єкта класу виключення
-        "Ділення на нуль"
-        Args:
-            num2(int): 0
+        "Ділення на нуль" (num2(int): 0)
         """
-        self.message = (f'помилка ділення на "0"')
+        self.message = 'помилка ділення на "0"'
 
     def __str__(self) -> str:
         """
@@ -21,33 +19,74 @@ class ValueZeroError(Exception):
 
 
 class Calculator:
+    """
+    Клас реалізує функції калкулятора
+    """
     def __init__(self) -> None:
+        """
+        Ініціалізатор об'єкта класу Calculator
+        """
         self.results_dict: dict = {}
 
     def __add__(self, num: int, num2: int) -> None:
+        """
+        Метод додавання
+        Args:
+            num (int): перше число
+            num2 (int): друге число
+        """
         result = num + num2
         self.results_dict[f"{num} + {num2}"] = result
         # print(self.results_dict)
         # print(type(self.results_dict))
 
-    def subtraction(self, num: int, num2: int):
+    def subtraction(self, num: int, num2: int) -> None:
+        """
+        Метод віднімання
+        Args:
+            num (int): 1 число
+            num2 (int): 2 число
+        """
         result = num - num2
         self.results_dict[f"{num} - {num2}"] = result
         # print(self.results_dict)
 
     def division(self, num: int, num2: int) -> None:
+        """
+        Метод звічайного ділення
+        Args:
+            num (int): 1 число
+            num2 (int): 2 число
+        Raises:
+            ValueZeroError: помилка ділення на 0
+        """
         if num2 == 0:
-            raise ValueZeroError("You can't divide by zero")
+            raise ValueZeroError()
         else:
             result = num / num2
             self.results_dict[f"{num} / {num2}"] = result
             # print(self.results_dict)
 
     def multiplication(self, num: int, num2: int) -> None:
+        """
+        Метод множення
+        Args:
+            num (int): 1 число
+            num2 (int): 2 число
+        """
         result = num * num2
         self.results_dict[f"{num} * {num2}"] = result
 
     def integer_division(self, num: int, num2: int) -> None:
+        """
+        Метод цілочисленого ділення
+        Args:
+            num (int): 1 число
+            num2 (int): 2 число
+
+        Raises:
+            ValueZeroError: помилка ділення на 0
+        """
         if num2 == 0:
             raise ValueZeroError("You can't divide by zero")
         else:
@@ -55,6 +94,15 @@ class Calculator:
             self.results_dict[f"{num} // {num2}"] = result
 
     def remainder_from_division(self, num: int, num2: int) -> None:
+        """
+        Метод залишок від ділення
+        Args:
+            num (int): 1 число
+            num2 (int): 2 число
+
+        Raises:
+            ValueZeroError: помилка ділення на 0
+        """
         if num2 == 0:
             raise ValueZeroError("You can't divide by zero")
         else:
@@ -63,17 +111,38 @@ class Calculator:
         # print(self.results_dict)
 
     def exponentiation(self, num: int, num2: int) -> None:
+        """
+        Метод піднесення до степеня
+        Args:
+            num (int): 1 число
+            num2 (int): 2 число (степень)
+        """
         result = pow(num, num2)
         self.results_dict[f"{num} ** {num2}"] = result
         # print(self.results_dict)
 
     def get(self) -> dict:
+        """
+        Метод для отримання словника
+        Returns:
+            dict: словник з прикладами
+        """
         return self.results_dict
 
 
 a = Calculator()
-a.division(12, 9)
-a.division(12, 9)
-a.__add__(12,30)
+a.integer_division(10, 2)
+a.division(1, 12)
+a.multiplication(1, 2)
+a.__add__(10, 23)
+a.subtraction(30, 10)
+a.integer_division(15, 2)
 
-print(a.get())
+
+
+
+
+
+
+
+# print(a.get())
