0b3d9e0128dfdd10486272822f94f6c82871fcb4
Mariia Sashchuk <160604681+theunforgiven7@users.noreply.github.com>
2024-06-11 15:37:52 +0300

    Update main18.py

---
+++ Hw18/main18.py
@@ -0,0 +1,32 @@
+from sqlite3 import connect
+
+
+with connect('C:/Users/mariy/Downloads/Chinook_Sqlite.sqlite') as con:
+    cur = con.cursor()
+    # 1
+    cur.execute('''SELECT * FROM Customer
+                                LIMIT 3''')
+    print(*cur.fetchall(), sep ='\n')
+
+    # 2
+    cur.execute('''SELECT SUM(Total)
+                                FROM Invoice''')
+    print(*cur.fetchone(), sep ='\n')
+
+    # 3
+    cur.execute('''SELECT *
+                            FROM Invoice
+                            WHERE BillingCity == "Paris"''')
+    print(*cur.fetchall(), sep ='\n')
+
+    # 4
+    cur.execute('''SELECT InvoiceDate
+                            FROM Invoice
+                            ORDER BY InvoiceDate ASC''')
+    print(*cur.fetchone())
+
+    # 5
+    cur.execute('''SELECT InvoiceDate
+                            FROM Invoice
+                            ORDER BY InvoiceDate DESC''')
+    print(*cur.fetchone())
